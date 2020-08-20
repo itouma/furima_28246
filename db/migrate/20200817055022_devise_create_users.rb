84b72b99ffa229717fd53b73a426239ee807e8c6
itouma <68621609+itouma@users.noreply.github.com>
2020-08-20 15:30:12 +0900

    migration修正

--- db/migrate/20200817055022_devise_create_users.rb
+++ db/migrate/20200817055022_devise_create_users.rb
@@ -9,9 +9,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :family_name,        null: false, default: ""
       t.string :first_name,         null: false, default: ""
-      t.string :family_name_kana    null: false, default: ""
+      t.string :family_name_kana,   null: false, default: ""
       t.string :first_name_kana,    null: false, default: ""
-      t.date   :birthday,           null: false, default: ""
+      t.date   :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_034934) do
+ActiveRecord::Schema.define(version: 2020_08_17_055022) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
+    t.string "name", default: "", null: false
+    t.integer "price", null: false
+    t.string "text", default: "", null: false
+    t.text "image", null: false
+    t.integer "category_id", null: false
+    t.integer "item_status_id", null: false
+    t.integer "cost_burden_id", null: false
+    t.integer "shipping_place_id", null: false
+    t.integer "ship_date_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "family_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
