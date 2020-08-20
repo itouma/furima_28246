6a851a285451064f3fd3fbbd17f27efc0eae138f
itouma <68621609+itouma@users.noreply.github.com>
2020-08-20 14:39:09 +0900

    users,itemsテーブルにカラムと制約追加

--- db/migrate/20200817034934_create_items.rb
+++ db/migrate/20200817034934_create_items.rb
@@ -1,9 +1,18 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :text
-      t.text :image
+      t.string :name               null: false, default: ""
+      t.integer :price             null: false, default: ""
+      t.string :text               null: false, default: ""
+
+      t.text :image                null: false, default: ""
+
+      t.integer :category_id	     null: false, default: ""
+      t.integer :item_status_id	   null: false, default: ""
+      t.integer :cost_burden_id    null: false, default: ""
+      t.integer :shipping_place_id null: false, default: ""
+      t.integer :ship_date_id	     null: false, default: ""
+      t.integer :user_id           not:false, foreign_key: true
       t.timestamps
     end
   end
