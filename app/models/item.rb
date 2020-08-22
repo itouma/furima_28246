4afa70042994901a20f10692e3f053df5f01092e
itouma <68621609+itouma@users.noreply.github.com>
2020-08-22 23:47:49 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
 
-  # with_options presence: true do
-  #   validates :name
-  #   validates :price
-  #   validates :text
+# PASSWORD_REGEX = [3-9][0-9]{2}|[1-9][0-9]{3,6}.freeze
+#半角英数字
 
-  #   validates :category_id
-  #   validates :item_status_id
-  #   validates :cost_burden_id
-  #   validates :shipping_place_id
-  #   validates :ship_date_id    
-  #   validates :user_id    
-  # end
+
+  with_options presence: { message: "can't be blank" }do
+    validates :name
+    validates :text
+    validates :price, length: { in: 300..9999999, message: "Out of setting range" }, numericality: { only_integer: true }
+    # 全角NG 半角数字のみ?できたか後でチェック
+  end
+
+  with_options presence: { message: "Select" }do
+    validates :category_id
+    validates :item_status_id
+    validates :cost_burden_id
+    validates :shipping_place_id
+    validates :ship_date_id    
+  end
+  validates :user_id, presence:true
 
 end
