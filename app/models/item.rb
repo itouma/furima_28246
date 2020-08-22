bccaad03225485bd8ca9d6a28c390f5923cbcda9
itouma <68621609+itouma@users.noreply.github.com>
2020-08-22 18:21:17 +0900

    ユーザーのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+
+  # with_options presence: true do
+  #   validates :name
+  #   validates :price
+  #   validates :text
+
+  #   validates :category_id
+  #   validates :item_status_id
+  #   validates :cost_burden_id
+  #   validates :shipping_place_id
+  #   validates :ship_date_id    
+  #   validates :user_id    
+  # end
+
 end
