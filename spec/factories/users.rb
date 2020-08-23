e83f9d0f52250331f4e85c7277964faf434a55fc
itouma <68621609+itouma@users.noreply.github.com>
2020-08-23 17:02:31 +0900

    無駄なコメントアウト修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -14,23 +14,3 @@ FactoryBot.define do
     
   end
 end
-
-# Faker::Number.number(digits: 10)
-# { .}
-# with_options presence: { message: "can't be blank" } do
-#   validates :nickname, length: { maximum: 6 }
-#   validates :
-#   with_options format: {with:HIRAGANA_KATAKANA_REGEX, message: "Full-width characters" } do
-#     validates :
-#     validates :
-#   end
-#   with_options format: {with:KATAKANA_REGEX, message:"Full-width katakana characters" } do
-#     validates :
-#     validates :
-#   end
-# end
-# validates :password,format:{with:PASSWORD_REGEX, message: "Include both letters and numbers"  }
-# # length: {minimum: 6, message:"6文字以上入力してください"}
-# # validates :email, uniqueness: {message: 'そのメールアドレスは登録済みです'}
-#   # emailの初期validatesでpresence:trueとuniqueness: true と@を含む必要があることが設定されてある
-#   # passwordもpresence:trueと6文字以上であることが初期validatesに設定済みである
