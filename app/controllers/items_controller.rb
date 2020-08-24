f60f86e58e23520341dc126785792a091ec81cd2
itouma <68621609+itouma@users.noreply.github.com>
2020-08-25 02:36:39 +0900

    ログインユーザだけ出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index,:show]
-  
+  # before_action :move_to_index, except: [:index,:show]
+    skip_before_action :authenticate_user!, only: [:index, :show ]
   def index
     @items = Item.all
     @purchases = Purchase.includes(:purchase)
@@ -25,11 +25,11 @@ class ItemsController < ApplicationController
 
   private
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  # def move_to_index
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
   
   def message_params
     params.require(:item).permit(:name, :price, :image).merge(user_id: current_user.id)
