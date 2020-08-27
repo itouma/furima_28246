3e20965659b4bb95def6eea3cb5b77b8347c707a
itouma <68621609+itouma@users.noreply.github.com>
2020-08-27 20:03:03 +0900

    エラー起きた場合の画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,9 @@ class ItemsController < ApplicationController
   def edit
   end
   
-  def updata
-    item = Item.find(params[:id])
-    if item.update(item_params)
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
