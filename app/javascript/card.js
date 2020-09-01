330585433f1f62e211e8dc4bb7769547cb5adfff
itouma <68621609+itouma@users.noreply.github.com>
2020-09-01 11:57:11 +0900

    クレジットエラーメッセージ 出す為削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -29,10 +29,6 @@ const pay = () => {
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
       } else {
-        const token = '';
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
       }
