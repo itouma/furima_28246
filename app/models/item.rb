fc9ed23ff2358f0a16831ad6948db51c29654c09
itouma <68621609+itouma@users.noreply.github.com>
2020-09-04 03:50:17 +0900

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,9 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_one :purchase
 
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
+  
   belongs_to_active_hash :category
   belongs_to_active_hash :item_status
   belongs_to_active_hash :cost_burden
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_082923) do
+ActiveRecord::Schema.define(version: 2020_09_03_182852) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_08_29_082923) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "item_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_item_tag_relations_on_item_id"
+    t.index ["tag_id"], name: "index_item_tag_relations_on_tag_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.integer "price", null: false
@@ -43,12 +52,6 @@ ActiveRecord::Schema.define(version: 2020_08_29_082923) do
     t.integer "prefecture_id", null: false
     t.integer "ship_date_id", null: false
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -73,6 +76,12 @@ ActiveRecord::Schema.define(version: 2020_08_29_082923) do
     t.index ["purchase_id"], name: "index_shipping_addresses_on_purchase_id"
   end
 
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -92,5 +101,7 @@ ActiveRecord::Schema.define(version: 2020_08_29_082923) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "item_tag_relations", "items"
+  add_foreign_key "item_tag_relations", "tags"
   add_foreign_key "shipping_addresses", "purchases"
 end
